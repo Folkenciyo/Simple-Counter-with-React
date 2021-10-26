70de54edff56e4a354fb53c785cfb6aac3c2fcc7
Folkenciyo <folkencillo@gmail.com>
2021-10-26 16:38:00 +0000

    counter is working, next buttons

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,24 +1,55 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-const Counter = () => {
-	const [seconds, setSeconds] = useState("0");
+const Counter = props => {
+	const [seconds, setSeconds] = useState(0);
+	const [isRunning, setIsRunning] = useState(false);
+
 	useEffect(() => {
-		window.setInterval(() => {
-			setSeconds(seconds => seconds + 1);
-		}, 1000);
-	}, []);
+		if (isRunning) {
+			const interval = setInterval(() => {
+				// creamos intervalos
+				if (seconds < 9) {
+					//si seconds es menor q 9, se le suma 1
+					setSeconds(seconds + 1);
+				} else {
+					//de lo contrario resetea
+					setSeconds(0);
+				}
+			}, 1000);
+		} else {
+			return () => clearInterval(interval); //mato intervalo
+		}
+	}, [seconds]); // keep variable SECONDS (PARRIBA)
 
 	return (
 		<div className="number">
-			<span
-				onClick={() => {
-					setSeconds(seconds);
-				}}>
-				{seconds}
-			</span>
+			<span>{seconds}</span>
+			<div className="buttons">
+				<button
+					className="play"
+					onClick={() => {
+						setIsRunning(true);
+					}}>
+					<i className="fas fa-play-circle"></i>
+				</button>
+				<button
+					className="pause"
+					onClick={() => {
+						setIsRunning(false);
+					}}>
+					<i className="fas fa-pause-circle"></i>
+				</button>
+				<button className="reset">
+					<i className="fas fa-power-off"></i>
+				</button>
+			</div>
 		</div>
 	);
+};
+
+Counter.PropType = {
+	start: PropTypes.number
 };
 
 export default Counter;
